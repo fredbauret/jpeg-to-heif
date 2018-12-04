29b8d1a02448374cdfa6dba5e67550aa7837f0d5
Fred Bauret <fred-bauret@adomos.com>
2018-12-04 16:29:57 +0100

    fix error in conversion script dir listing

--- bin/convert.sh
+++ bin/convert.sh
@@ -5,7 +5,7 @@ subdir=${1:-images}
 echo "converting in subdir: ./$subdir"
 
 cd /gpac/$subdir
-for F in *.[jJ][pP][gG] *.[jJ][pP][eE][gG]; do
+for F in $(ls *.[jJ][pP][gG] *.[jJ][pP][eE][gG] 2>/dev/null); do
   if ! [ -f $F.heic ]; then
     ffmpeg -i ./$F -crf 23 -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -preset slower -pix_fmt yuv420p -f hevc $F.hvc && \
     MP4Box -add-image $F.hvc -ab heic -new $F.heic && \
